8c94d824266d63ec2ebceb4e7b75ae77d1287347
Edwin Mesa Mac Samaria <edwinmesa@gmail.com>
2021-12-10 09:20:46 -0500

    Inicializando proyecto

---
+++ front/js/script.js
@@ -0,0 +1,35 @@
+const base_url = "http://localhost:3000"
+
+var form = document.getElementById('login_form');
+form.addEventListener('submit', login_event);
+
+function login_event(e) {
+
+  // prevent the browser redirection
+  e.preventDefault();
+  
+  var message = document.getElementById('message');
+  message.textContent = "";
+  
+  /* Get all inputs from the form */
+  var formData = new FormData(form);
+
+  /* Convert data into json */
+  var data = Object.fromEntries(formData);
+
+  const url_login = `${base_url}/login`;
+
+  fetch(url_login, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data),
+    }
+  )
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    form.reset();
+    message.textContent = data.message;
+  });
+
+}
